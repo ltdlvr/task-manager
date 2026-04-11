48142970a4441e09abec43d74380ebfbe213447f
Andrey Skripka <andreyskripka.work@gmail.com>
2026-04-11 22:11:58 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
+	"github.com/ltdlvr/task-manager/internal/db"
+	"github.com/ltdlvr/task-manager/internal/handlers"
+	"github.com/ltdlvr/task-manager/internal/repository"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	err := godotenv.Load() //я думал закинуть это в отдельный файл, но в описании функции написано, что ее в идеале в мейне вызывать
+	if err != nil {
+		log.Warn("No .env file found")
+	}
+
+	pool, err := db.InitDatabase()
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	} else {
+		log.Info("Database initialized")
+	}
+	repo := &repository.UserRepository{Pool: pool}
+	userHandler := &handlers.UserHandler{Repo: repo}
+
+	app := fiber.New()
+
+	//TODO - добить эндпоинты регистрации
+
+	app.Get("/ping", handlers.HealthCheck)
+	app.Post("/register", userHandler.Register)
+	app.Listen(":3000")
+}
